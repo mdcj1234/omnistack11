9989c8808e4c596284f2a1114afca6082675bd81
Marcio Danilo Costa Junior <junior@encel.com.br>
2020-03-25 20:23:27 -0300

    Aula 2 Back-end

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,17 +1,25 @@
 const express = require('express');
+const crypto = require('crypto');
+const connection = require('./database/connection');
 
 const routes = express.Router();
 
-routes.post('/users', (request, response) => {
+routes.post('/ongs', async (request, response) => {
 
-    const body = request.body;
+    const {name, email, whatsapp, city, uf} = request.body;
 
-    console.log(body);
+    const id = crypto.randomBytes(4).toString('HEX');
 
-    return response.json({
-        evento : 'Semana Omnistack 11.0',
-        aluno : 'Marcio Jr',
-    });
+    await connection('ongs').insert({
+        id,
+        name,
+        email,
+        whatsapp,
+        city,
+        uf,
+    })
+
+    return response.json({ id });
 });
 
 module.exports = routes;
