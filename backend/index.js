d52ac3875a2bb06549852360ac17d7b976f8695f
Marcio Junior <mdcj1234@gmail.com>
2020-03-24 23:39:46 -0300

    Preparando o ambiente

--- backend/index.js
+++ backend/index.js
@@ -2,9 +2,39 @@ const express = require('express');
 
 const app = express();
 
-app.get('/', (request, response) => {
+app.use(express.json());
+
+/**
+ * Métodos HTPP:
+ * 
+ * GET: Buscar uma informação no back-end
+ * POST: Criar uma informação no back-end
+ * PUT: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+/**
+ * Parâmetros:
+ * 
+ * Query Params: Parâmetros nomeados enviados na rota após '?'
+ * Route Params: Parâmetros utilizados para identificar recursos ':'
+ * Request Body: Corpo da requisição. Usado para criar ou alterar recursos.
+ */
+
+ /**
+  * Relacionais
+  * Não Relacionais 
+  */
+
+  
+app.post('/users', (request, response) => {
+
+    const body = request.body;
+
+    console.log(body);
+
     return response.json({
-        evento : 'Omnistack',
+        evento : 'Semana Omnistack 11.0',
         aluno : 'Marcio Jr',
     });
 })
