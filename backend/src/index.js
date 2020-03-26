387f5be8012b393d05c981d60dc55050b0e33913
Marcio Danilo Costa Junior <junior@encel.com.br>
2020-03-25 21:35:15 -0300

    Final Aula 2

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,33 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes');
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
-
-/**
- * Métodos HTPP:
- * 
- * GET: Buscar uma informação no back-end
- * POST: Criar uma informação no back-end
- * PUT: Alterar uma informação no back-end
- * DELETE: Deletar uma informação no back-end
- */
-
-/**
- * Parâmetros:
- * 
- * Query Params: Parâmetros nomeados enviados na rota após '?'
- * Route Params: Parâmetros utilizados para identificar recursos ':'
- * Request Body: Corpo da requisição. Usado para criar ou alterar recursos.
- */
-
- /**
-  * Banco de Dados:
-  * 
-  * Relacionais
-  * Não Relacionais 
-  */
 
 app.listen(3333);
